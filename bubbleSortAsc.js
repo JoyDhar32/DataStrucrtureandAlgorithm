f0be96a6c8acca8e1813215be444aaeb1746a230
JoyDhar32 <joyustcian@gmail.com>
2023-12-22 00:49:28 +1100

    bubbleSortAsc

---
+++ bubbleSortAsc.js
@@ -0,0 +1,21 @@
+const BubbleSort = (arr) => {
+    let swapped;
+    do {
+      swapped = false;
+  
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {  //Descending order
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+        }
+      }
+    } while (swapped);
+    return arr;
+  };
+  console.log(BubbleSort([8, 20, -2, 4, -6]));
+  console.log(BubbleSort([55, 20, -22, 14, -69]));
+  console.log(BubbleSort([8, 20, -22, 14, -69]));
+  console.log(BubbleSort([81, 4, -44, 44, 4]));
+  
