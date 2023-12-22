1667bb720a085e3b9ca6d31fe52c2d3fe3e09113
JoyDhar32 <joyustcian@gmail.com>
2023-12-22 15:54:57 +1100

    adding insertion docx

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,3 +1,24 @@
+/*
+Start with the second element (assume the first element is already sorted):
+
+The first element is considered sorted since there are no preceding elements.
+Compare and insert:
+
+Take the second element and compare it with the first.
+If the second element is smaller, swap them.
+Extend the sorted portion:
+
+Move to the third element.
+Compare it with the second element and swap if necessary.
+Compare it with the first element and swap if necessary.
+Continue the process:
+
+Continue this process for the remaining elements, extending the sorted portion of the array with each iteration.
+Final sorted array:
+
+After iterating through all elements, the array is sorted.
+*/
+
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i]
