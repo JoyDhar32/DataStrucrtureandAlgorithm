edb729fea2b248ab1e6b2933bd489ce4b188914c
JoyDhar32 <joyustcian@gmail.com>
2024-01-11 14:26:33 +1100

    modify

--- array.js
+++ array.js
@@ -1,2 +1,5 @@
 const ar=[1,2,3,'joy',true,{'name':'joy','age':23}];
 console.log(ar[5]['name'])
+
+const arr=[1,2,3,'joy',true,{'name':'joy','age':23}];
+console.log(arr[5]['name'])
--- methods/stringtonumberviceversa.js
+++ methods/stringtonumberviceversa.js
@@ -9,3 +9,5 @@ console.log(back); // This will dynamically convert the string to a number every
 console.log(typeof back); // This will show you that the variable is a number
 console.log(usingParse); // This will dynamically convert the string to a number every time you call usingParse()
 console.log(typeof usingParse); // This will show you that the variable is a number
+
+// Path: methods/trim.js
