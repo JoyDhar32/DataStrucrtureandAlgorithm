02e1b263b56900ed0988e7c67ebb1df9f5760e81
JoyDhar32 <joyustcian@gmail.com>
2024-01-20 21:24:42 +1100

    ⭐ time complexity:O(nlogn)

--- QuickSort.js
+++ QuickSort.js
@@ -20,9 +20,4 @@ console.log(QuickSort([1,1,1,1,1,1,1,1,1,1])); // [1,1,1,1,1,1,1,1,1,1]
 console.log(QuickSort([1,1,1,1,1,1,1,1,1,2])); // [1,1,1,1,1,1,1,1,1,2]
 console.log(QuickSort([2,1,1,1,1,1,1,1,1,1])); // [1,1,1,1,1,1,1,1,1,2]
 
-//⭐ time complexity: O(nlogn)
-
-
-
-
-
+//⭐time complexity: O(nlogn)
