f415e6049118952d643deaed18ea7e941c8ad2e6
JoyDhar32 <joyustcian@gmail.com>
2024-04-14 15:34:14 +1000

    ⭐ Using For, Map and Foreach loop, we can get the same output. But using map is more efficient and easy to understand. ⭐

--- Coding Challenges/Capital_First_Word.js
+++ Coding Challenges/Capital_First_Word.js
@@ -1,30 +1,30 @@
-let students =[
-    
-        {name: "John", age: 25, city: "New York"}, 
-        {name: "Jane", age: 22, city: "San Francisco"},
-        {name: "Robarn", age: 25, city: "Victoria"},
-        {name: "Micheal", age: 22, city: "New York"},
-        {name: "Rodge", age: 28, city: "California"},
+let students = [
+
+    { name: "John", age: 25, city: "New York" },
+    { name: "Jane", age: 22, city: "San Francisco" },
+    { name: "Robarn", age: 25, city: "Victoria" },
+    { name: "Micheal", age: 22, city: "New York" },
+    { name: "Rodge", age: 28, city: "California" },
 ]
 
 // Capitalize the first letter of each word in the city name
 // Expected Output: JOHN, JANE, ROBARN, MICHEAL, RODGE
 
 //Example 1
-let names=[];
-for(let i=0;i<students.length;i++){
+let names = [];
+for (let i = 0; i < students.length; i++) {
     names.push(students[i].name.toUpperCase())
 }
 console.log(names);
 
 //Example 2
 
-let names2 = students.map(student=>student.name.toUpperCase())
+let names2 = students.map(student => student.name.toUpperCase())
 console.log(names2);
 
 // Example 3 using foreach
-let names3=[];
-students.forEach(student=>names3.push(student.name.toUpperCase()))
+let names3 = [];
+students.forEach(student => names3.push(student.name.toUpperCase()))
 console.log(names3);
 
 // ⭐ Using For, Map and Foreach loop, we can get the same output. But using map is more efficient and easy to understand. ⭐ 
