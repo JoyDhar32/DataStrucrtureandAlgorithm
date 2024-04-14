ac00546bbb1c7123f0dec9c956daffaa3fa533a2
JoyDhar32 <joyustcian@gmail.com>
2024-04-14 15:51:30 +1000

    ⭐ Converting decimal to all types ⭐

---
+++ Coding Challenges/Number_conversion.js
@@ -0,0 +1,8 @@
+let decimal=22;
+let binary=decimal.toString(2);
+let octal=decimal.toString(8);
+let hexadecimal=decimal.toString(16);
+
+console.log(` Decimal: ${decimal} \n Binary: ${binary} \n Octal: ${octal} \n Hexadecimal: ${hexadecimal}`);
+
+// ⭐ Converting decimal to all types ⭐ 
